00d1ee8e9f815d9428261d52f037e21799f09d2c
JohnBroda <161051910+JohnBroda@users.noreply.github.com>
2024-05-08 11:52:27 +0200

    PF v.0.0.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { StartComponent } from './start/start.component'; 
 import { PortfolioComponent } from './portfolio/portfolio.component'; 
 import { AboutMeComponent } from './about-me/about-me.component'; 
 import { ContactComponent } from './contact/contact.component'; 
@@ -12,7 +11,7 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, StartComponent, PortfolioComponent, AboutMeComponent, ContactComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, PortfolioComponent, AboutMeComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
