5789f778c655ed7b81c68e7afe0dc007fa852336
JohnBroda <161051910+JohnBroda@users.noreply.github.com>
2024-05-15 20:13:08 +0200

    PF v0.5

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  showErrorMessage: boolean = false;
+
+  contactData={
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit(contactForm: NgForm){
+    if(contactForm.valid){
+      console.log(this.contactData)
+    }
+    
+  }
 
 }
