dd9a83120c4094b03e1fb9d15d385666eabc9169
JohnBroda <161051910+JohnBroda@users.noreply.github.com>
2024-05-17 19:30:36 +0200

    PF v0.8.5

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -14,14 +14,19 @@ export class PortfolioComponent {
       title: 'Join',
       subtitle: 'JavaScript | HTML | CSS',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      imageUrl: './../../assets/img/portfolio/join.png'
+      imageUrl: './../../assets/img/portfolio/join.png',
+      serverLink: '',
+      gitLink:'https://github.com/Simon-Kral/join',
+
     },
     {
       direction: 'TR',
       title: 'El Pollo Loco',
       subtitle: 'JavaScript | HTML | CSS',
       description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
-      imageUrl: './../../assets/img/portfolio/elpolloloco.png'
+      imageUrl: './../../assets/img/portfolio/elpolloloco.png',
+      serverLink: '',
+      gitLink:'https://github.com/Simon-Kral/join',
     },
   ];
 }
