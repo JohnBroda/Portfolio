d1ea435902c10cc59d15b62df8df053174f7d28c
JohnBroda <161051910+JohnBroda@users.noreply.github.com>
2024-05-27 12:10:03 +0200

    PF v.1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule, ViewportScroller } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { PortfolioComponent } from './portfolio/portfolio.component'; 
-import { AboutMeComponent } from './about-me/about-me.component'; 
-import { ContactComponent } from './contact/contact.component'; 
 import { FooterComponent } from './footer/footer.component'; 
-import { StartComponent } from './start/start.component';
-import { SkillsComponent } from './skills/skills.component';
 import { MainContentComponent } from './main-content/main-content.component';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainContentComponent, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [MainContentComponent, RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit {
+  showbgObject: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const hiddenRoutes = ['/Impressum', '/PrivacyPolicy'];
+        this.showbgObject = !hiddenRoutes.includes(event.url);
+      }
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { NgModule } from '@angular/core';
+
+
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
     { path: 'Impressum', component: ImpressumComponent },
     { path: 'PrivacyPolicy', component: PrivacyPolicyComponent }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
